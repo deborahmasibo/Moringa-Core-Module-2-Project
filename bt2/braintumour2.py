66d6d9bd47fd24358c8ac94c7b2997af532b4832
Deborah Masibo <98378517+deborahmasibo@users.noreply.github.com>
2022-05-18 12:45:55 +0300

    Update braintumour2.py

--- bt2/braintumour2.py
+++ bt2/braintumour2.py
@@ -90,14 +90,14 @@ def PageSpecifications(sBox):
                 col1, col2 = st.columns([5, 5])
                 # st.image(image, caption='Uploaded Image', use_column_width=True, width = 200)
                 # class_btn = st.button("Generate Prediction")
-                # if class_btn:                    
-                with st.spinner('Model working....'):
-                        with col1:
-                            predictions = predict(image)
-                        with col2:
-                            time.sleep(1)
-                            st.success('Classified')
-                            st.markdown("<h5 style='text-align: left; color: black;'> {} </h5>".format(predictions), unsafe_allow_html=True)
+                # if class_btn:                  
+                with col1:
+                    with st.spinner('Model working....'):
+                    predictions = predict(image)
+                with col2:
+                    time.sleep(1)
+                    st.success('Classified')
+                    st.markdown("<h5 style='text-align: left; color: black;'> {} </h5>".format(predictions), unsafe_allow_html=True)
                             
 
         def predict(image):
