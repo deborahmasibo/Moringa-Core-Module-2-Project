0f61171a15f382ee99812f6d785db9b73f701470
Deborah Masibo <98378517+deborahmasibo@users.noreply.github.com>
2022-05-20 09:23:15 +0300

    Update braintumour2.py

--- bt2/braintumour2.py
+++ bt2/braintumour2.py
@@ -138,12 +138,12 @@ PageSpecifications()
 st.markdown("<h5 style='text-align: center; color: red;'> Disclaimer! </h1>", unsafe_allow_html=True)
 col1, col2, col3 = st.columns([1,1,1])
 with col1:
-            pass
+    st.write('')
 with col2:
-            st.write('''The application should only be used for educational purporses, and not for self-diagnosis or any other personal use. 
-            For real life cases, consult your local medical authority for advice.''')
+    st.write('''The application should only be used for educational purporses, and not for self-diagnosis or any other personal use. 
+              For real life cases, consult your local medical authority for advice.''')
 with col3:
-           pass 
+     pass 
 #------------------------------------------------------------------------------------------------------------------------------------------
 
     
